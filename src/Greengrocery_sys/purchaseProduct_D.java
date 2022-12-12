cf7979b6c8129a9e63e6f3f6ef99b7095ae1e64d
TSS <User@TSS>
2022-12-12 15:29:39 +0630

    Add Product

--- src/Greengrocery_sys/purchaseProduct_D.java
+++ src/Greengrocery_sys/purchaseProduct_D.java
@@ -6,6 +6,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.TableColumnModel;
+
+import Greengrocery_sys.Database.DbConnection;
+import net.proteanit.sql.DbUtils;
+
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.ImageIcon;
@@ -21,6 +26,12 @@ import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class purchaseProduct_D extends JFrame {
 
@@ -47,13 +58,35 @@ public class purchaseProduct_D extends JFrame {
 			}
 		});
 	}
+	
+	/*
+	 * refresh the table
+	 */
+	private void refreshTable() {
+		Connection connection = new DbConnection().connect();
+		String sqlString = "select * from purchase_price";
+		try {
+			PreparedStatement pStatement = connection.prepareStatement(sqlString);
+			ResultSet rSet = pStatement.executeQuery();
+			
+			tbPurchase.setModel(DbUtils.resultSetToTableModel(rSet));
+			
+			TableColumnModel columnModel = tbPurchase.getColumnModel();
+			columnModel.removeColumn(columnModel.getColumn(0));
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 	/**
 	 * Create the frame.
 	 */
 	public purchaseProduct_D() {
+		setUndecorated(true);
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1106, 506);
+		setBounds(100, 100, 1106, 476);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -61,9 +94,31 @@ public class purchaseProduct_D extends JFrame {
 		contentPane.setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(0, 0, 1090, 467);
+		panel.setBounds(0, 0, 1117, 476);
 		contentPane.add(panel);
 		panel.setLayout(null);
+		
+		JButton btnMinimize = new JButton("-");
+		btnMinimize.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				purchaseProduct_D.this.setExtendedState(JFrame.ICONIFIED);
+			}
+		});
+		btnMinimize.setFont(new Font("Tahoma", Font.BOLD, 23));
+		btnMinimize.setBackground(new Color(214, 54, 31));
+		btnMinimize.setBounds(1022, 0, 44, 34);
+		panel.add(btnMinimize);
+		
+		JButton btnClose = new JButton("X");
+		btnClose.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				purchaseProduct_D.this.setVisible(false);
+			}
+		});
+		btnClose.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnClose.setBackground(new Color(214, 54, 31));
+		btnClose.setBounds(1063, 0, 44, 34);
+		panel.add(btnClose);
 		
 		JButton btnDelete = new JButton("ဖ်က္မည္");
 		btnDelete.setForeground(new Color(128, 64, 0));
@@ -73,6 +128,18 @@ public class purchaseProduct_D extends JFrame {
 		panel.add(btnDelete);
 		
 		JButton btnAdd = new JButton("ထည့္မည္");
+		btnAdd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String typeString = cBoxSize.getSelectedItem().toString();
+				int bucketP = Integer.parseInt(txtBacketPrice.getText());
+				int boxP = Integer.parseInt(txtBoxPrice.getText());
+				int cardP = Integer.parseInt(txtCardPrice.getText());
+				int vissP = Integer.parseInt(txtVissPrice.getText());
+				
+				new addPurchase_F().addPurchase(typeString, bucketP, boxP, cardP, vissP);
+				refreshTable();
+			}
+		});
 		btnAdd.setForeground(new Color(128, 64, 0));
 		btnAdd.setFont(new Font("Zawgyi-One", Font.BOLD, 15));
 		btnAdd.setBackground(new Color(254, 251, 245));
@@ -112,7 +179,7 @@ public class purchaseProduct_D extends JFrame {
 				txtVissPrice.setText("");
 			}
 		});
-		txtVissPrice.setText("0.00");
+		txtVissPrice.setText("0");
 		txtVissPrice.setFont(new Font("Times New Roman", Font.BOLD, 16));
 		txtVissPrice.setColumns(10);
 		txtVissPrice.setBackground(new Color(254, 251, 245));
@@ -146,6 +213,18 @@ public class purchaseProduct_D extends JFrame {
 		panel.add(txtBoxPrice);
 		
 		txtCardPrice = new JTextField();
+		txtCardPrice.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				char c = arg0.getKeyChar();
+				if(Character.isLetter(c)) {
+					JOptionPane.showMessageDialog(null, "Please enter the NUMBER (1,2,3,...) only.", "Warning!",JOptionPane.INFORMATION_MESSAGE);
+				}
+				if(arg0.getKeyCode() == KeyEvent.VK_ENTER) {
+					txtVissPrice.requestFocus();
+				}
+			}
+		});
 		txtCardPrice.addFocusListener(new FocusAdapter() {
 			@Override
 			public void focusGained(FocusEvent e) {
@@ -236,15 +315,26 @@ public class purchaseProduct_D extends JFrame {
 		scrollPane.setBounds(20, 66, 749, 366);
 		panel.add(scrollPane);
 		
-		tbPurchase = new JTable();
+		tbPurchase = new JTable() {
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+		};
+		tbPurchase.setFont(new Font("Zawgyi-One", Font.PLAIN, 12));
+		tbPurchase.setRowHeight(30);
+		tbPurchase.getTableHeader().setFont(new Font("Verdana", Font.BOLD, 15));
+		tbPurchase.getTableHeader().setBackground(new Color(128, 64, 0));
+		tbPurchase.getTableHeader().setForeground(Color.white);
 		scrollPane.setViewportView(tbPurchase);
 		
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setOpaque(true);
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\User\\git\\Green_grocery\\Images\\tomato.jpg"));
-		lblNewLabel.setBounds(0, 0, 1080, 467);
+		lblNewLabel.setBounds(0, 0, 1107, 476);
 		lblNewLabel.setBackground(new Color(0,28,0));
 		panel.add(lblNewLabel);
+		
+		refreshTable();
 	}
 }
